e3dfca62df9cc59e7ae918d637bafa901e99f08b
Pibo Shongwe <piboman@yahoo.com>
2024-07-10 03:34:35 +0000

    update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -16,11 +16,37 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    """GET /api/v1/stats
+    Return: the number of objects
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """GET /api/v1/stats
+    Return: the number of each objects.
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return: Unauthorized error.
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return: Forbidden error.
+    """
+    abort(403)
