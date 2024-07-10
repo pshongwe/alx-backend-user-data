91c9a2090e626f3d25bf99a27972683f794a2700
Pibo Shongwe <piboman@yahoo.com>
2024-07-10 03:51:11 +0000

    u

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,18 +24,6 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """GET /api/v1/stats
-    Return: the number of each objects.
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
-
-
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
     """GET /api/v1/unauthorized
