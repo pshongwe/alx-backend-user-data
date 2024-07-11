d10ca5143632d0e5027204654665c9e64884b3f1
Pibo Shongwe <piboman@yahoo.com>
2024-07-11 22:10:22 +0000

    u

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ find user id by session id"""
+        if session_id is None or isinstance(session_id, str) is False:
+            return None
+        return self.user_id_by_session_id.get(session_id)
