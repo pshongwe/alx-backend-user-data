eaa6f953603da13454dbfa1da210c63ce9181627
Pibo Shongwe <piboman@yahoo.com>
2024-07-11 22:44:02 +0000

    u

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,13 +56,15 @@ def authenticate_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
             request.current_user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
+            if auth.authorization_header(request) is None:
+                if auth.session_cookie(request) is None:
+                    return None, abort(401)
             if user is None:
                 abort(403)
             request.current_user = user
