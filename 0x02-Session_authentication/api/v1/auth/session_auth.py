39c6c863d26a0b0a918bebdfc9d9e834318deed5
Pibo Shongwe <piboman@yahoo.com>
2024-07-11 23:45:19 +0000

    u

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 api/v1/auth/session_auth.py
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -23,3 +24,10 @@ class SessionAuth(Auth):
         if session_id is None or isinstance(session_id, str) is False:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """
+        Returns a User instance based on a cookie value.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
