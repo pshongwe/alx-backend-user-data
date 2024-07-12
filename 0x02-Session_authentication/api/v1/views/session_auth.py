af60aa3a9cee3f6918df7a07cc987aaed950691e
Pibo Shongwe <piboman@yahoo.com>
2024-07-12 00:05:58 +0000

    u

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Session authentication view module
+"""
+from flask import Blueprint, request, jsonify, abort
+from models.user import User
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.views import app_views
+from typing import Tuple
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    """
+    Handle login route for session authentication
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or len(email.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
+
+    if not password or len(password.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    session_name = os.getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
+
+    return response
