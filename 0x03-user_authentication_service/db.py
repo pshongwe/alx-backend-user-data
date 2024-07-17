aff08db375683a8422dff0489f6c2a664642c231
Pibo Shongwe <piboman@yahoo.com>
2024-07-17 20:26:11 +0000

    task 1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self) -> User:
+        """Add user to database"""
+        newUser = User(email=email, hashed_password=hashed_password)
+        try:
+            self._session.add(newUser)
+            self._session.commit()
+        except Exception as ex:
+            print(f"Error adding user to the database: {ex}")
+            self._session.rollback()
+            raise
+        return newUser
