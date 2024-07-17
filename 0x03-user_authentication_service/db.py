d034b06605a8eb5ae5f918594b76c268f437a80e
Pibo Shongwe <piboman@yahoo.com>
2024-07-17 20:28:57 +0000

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add user to database"""
         newUser = User(email=email, hashed_password=hashed_password)
         try:
