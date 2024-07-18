df8593de8d8a5bf2669d10c87d6dc53a5f2c0e3a
Pibo Shongwe <piboman@yahoo.com>
2024-07-18 17:17:54 +0000

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,34 @@
 Auth file
 """
 import bcrypt
+from db import DB, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Create password Hash"""
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user"""
+        try:
+            cur_user = self._db.find_user_by(email=email)
+
+            if cur_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hp = _hash_password(password)
+        newUser = self._db.add_user(
+               email=email, hashed_password=hp.decode('utf-8'))
+        return newUser
