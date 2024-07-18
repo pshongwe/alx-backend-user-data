d63dcb353e662d98e61066d97c9fbc203de8ff7e
Pibo Shongwe <piboman@yahoo.com>
2024-07-18 17:59:22 +0000

    u

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,15 @@ class Auth:
         newUser = self._db.add_user(
                email=email, hashed_password=hp.decode('utf-8'))
         return newUser
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if a user's login details are valid.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode("utf-8"),
+                                       user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
