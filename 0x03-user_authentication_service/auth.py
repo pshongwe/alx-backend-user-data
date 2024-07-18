23716ef1e5343a9a4115b09c26479e6da8bae556
Pibo Shongwe <piboman@yahoo.com>
2024-07-18 20:36:25 +0000

    u

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ Auth file
 import bcrypt
 from db import DB, User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,6 +13,9 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt())
 
+def _generate_uuid() -> str:
+    """Generate a uuid"""
+     return str(uuid4())
 
 class Auth:
     """Auth class to interact with the authentication database.
